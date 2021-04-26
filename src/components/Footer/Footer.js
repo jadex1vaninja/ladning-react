cf1608254b79b6277ffc839674c5167f1a3c91fa
Ivaninja <wanyaka1@gmail.com>
2021-04-26 18:42:29 +0300

    added footer img

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import { useTranslation } from 'react-i18next';
 import { dazn_link } from '../../const';
 import { imgPath } from '../Landing/Landing';
 import './Footer.scss';
 
 const Footer = () => {
-
+  const { i18n } = useTranslation();
+  const language = i18n.language;
+  const IS_SPANISH = language === 'es';
   return (
     <footer className='footer'>
       <div className='footer__banner'>
@@ -14,18 +17,24 @@ const Footer = () => {
           alt='footer'
         />
         <div className='footer__face-container'>
-          <img
-            className='footer__face-img'
-            src={imgPath + 'face-fearless.png'}
-            alt='face the fearless'
-          />
+          {
+            <img
+              className='footer__face-img'
+              src={
+                IS_SPANISH
+                  ? imgPath + 'face-fearless-es.png'
+                  : imgPath + 'face-fearless.png'
+              }
+              alt='face the fearless'
+            />
+          }
         </div>
         <div className='footer__inner'>
           <div className='footer__img-wrapper'>
             <img className='footer__logo1' src={imgPath + 'logo.png'} alt='' />
           </div>
           <div className='footer__text-wrapper'>
-            <a target="_blank" href={dazn_link}>
+            <a target='_blank' href={dazn_link}>
               <img
                 className='footer__logo2'
                 src={imgPath + 'DAZN-logo.png'}
