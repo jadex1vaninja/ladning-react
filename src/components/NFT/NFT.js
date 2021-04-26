5766d1ae5dbcd27fbba7caa23e73dafdbfe48ce8
jadex1vaninja <ivan.lazarev@jadexconsulting.com>
2021-04-26 12:04:08 +0300

    quick fix

--- src/components/NFT/NFT.js
+++ src/components/NFT/NFT.js
@@ -13,14 +13,11 @@ const NFT = ({ img, type, title, description, rarity }) => {
             {type}
           </h4>
           <h3 className='first-description'>{title}</h3>
-          <h3 className='second-description'>{description}</h3>
           <div className='cards-root__hr'></div>
         </div>
         <div className='cards-root__auction-wrapper'>
           <p className='cards-root__amount'>{rarity}</p>
-          <p className='cards-root__desc'>
-            {t('marketplace.ntfs.legendary.description')}
-          </p>
+          <p className='cards-root__desc'>{description}</p>
           <a className='cards-root__link' href=''>
             {t('marketplace.ntfs.legendary.link')}
           </a>
