651b7c6544915f511c1c38d288622bdbdbc36d3e
jadex1vaninja <ivan.lazarev@jadexconsulting.com>
2021-04-27 15:29:29 +0300

    changed img

--- src/components/OwnItSection/OwnItSection.js
+++ src/components/OwnItSection/OwnItSection.js
@@ -10,7 +10,10 @@ const OwnItSection = () => {
     <section className='own-it'>
       <div className='own-it__item-wrapper'>
         <div className='own-it__item'>
-          <img src={imgPath + 'nft-legend.png'} alt='card' />
+          <img
+            src={imgPath + 'Legendary_2_Knockout_Canelo_1_1.png'}
+            alt='card'
+          />
           <div className='own-it__item-content'>
             <h2>{t('own-it-block.title')}</h2>
             <p>{t('own-it-block.description-part-one')}</p>
