7fd77286cdbdd595a55e73218704300e6ef5c68e
jadex1vaninja <ivan.lazarev@jadexconsulting.com>
2021-04-27 14:51:41 +0300

    reatured section in progress

--- src/components/FeaturedNFT/FeaturedNFT.js
+++ src/components/FeaturedNFT/FeaturedNFT.js
@@ -10,10 +10,10 @@ const FeaturedNFT = () => {
 
   const renderer = ({ days, minutes, seconds }) => {
     return (
-      <div className='cards-root__time-section'>
-        <p className='cards-root__time-chunk'>
-          <span className='cards-root__time-value'>{days}</span>
-          <span className='cards-root__indicator'>
+      <div className='featured__time-section'>
+        <p className='featured__time-chunk'>
+          <span className='featured__time-value'>{days}</span>
+          <span className='featured__indicator'>
             {t('marketplace.countdown.days')}
           </span>
         </p>
@@ -23,15 +23,15 @@ const FeaturedNFT = () => {
         {/*    {t('marketplace.countdown.hours')}*/}
         {/*  </span>*/}
         {/*</p>*/}
-        <p className='cards-root__time-chunk'>
-          <span className='cards-root__time-value'>{minutes}</span>
-          <span className='cards-root__indicator'>
+        <p className='featured__time-chunk'>
+          <span className='featured__time-value'>{minutes}</span>
+          <span className='featured__indicator'>
             {t('marketplace.countdown.minutes')}
           </span>
         </p>
-        <p className='cards-root__time-chunk'>
-          <span className='cards-root__time-value'>{seconds}</span>
-          <span className='cards-root__indicator'>
+        <p className='featured__time-chunk'>
+          <span className='featured__time-value'>{seconds}</span>
+          <span className='featured__indicator'>
             {t('marketplace.countdown.seconds')}
           </span>
         </p>
@@ -41,27 +41,29 @@ const FeaturedNFT = () => {
 
   return (
     <div className='featured'>
-      <div className='featured__img-wrapper'>
-        <img src={imgPath + 'own-it.png'} alt='card' />
-      </div>
-      <div className='featured__content'>
-        <h6 className='featured__sub-title'>FEATURED NFT </h6>
-        <h1 className='featured__title'>KNOCKOUT – CANELO EDITION</h1>
-        <div className='featured__type'>
-          <h2 className='featured__rarity'>1 of 1</h2>
-          <h2 className='featured__edition featured__edition--legendary'>
-            Legendary Edition
-          </h2>
+      <div className='featured__inner-wrapper'>
+        <div className='featured__img-wrapper'>
+          <img src={imgPath + 'own-it.png'} alt='card' />
         </div>
-        <div className='featured__details'>
-          <div className='featured__price-block'>
-            <h2 className='featured__price-title'>Current price</h2>
-            <p className='featured__price-value'>40</p>
-            <p className='featured__price-sign'>ETH</p>
+        <div className='featured__content'>
+          <h6 className='featured__sub-title'>FEATURED NFT </h6>
+          <h1 className='featured__title'>KNOCKOUT – CANELO EDITION</h1>
+          <div className='featured__type'>
+            <h2 className='featured__rarity'>1 of 1</h2>
+            <h2 className='featured__edition featured__edition--legendary'>
+              Legendary Edition
+            </h2>
           </div>
-          <div className='featured__countdown-block'>
-            <h2>auction ending in</h2>
-            <Countdown date={date} daysInHours={true} renderer={renderer} />
+          <div className='featured__details'>
+            <div className='featured__price-block'>
+              <h2 className='featured__price-title'>Current price</h2>
+              <p className='featured__price-value'>40</p>
+              <p className='featured__price-sign'>ETH</p>
+            </div>
+            <div className='featured__countdown-block'>
+              <h2 className='featured__countdown-title'>Auction ending in</h2>
+              <Countdown date={date} daysInHours={true} renderer={renderer} />
+            </div>
           </div>
         </div>
       </div>
