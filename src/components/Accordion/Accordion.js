bb7adb84c55591774bb7ff2887d87e368e468374
CNBailey <80108020+CNBailey@users.noreply.github.com>
2021-05-05 11:54:37 +0100

    Revert "Merge branch 'dev' of https://github.com/jadex1vaninja/ladning-react into dev"
    This reverts commit cd1c8317f0670762a2dd5cc68e2919b001023496, reversing
    changes made to c8eabb3202866827d746a5d0c2792dca460ab5d8.

--- src/components/Accordion/Accordion.js
+++ src/components/Accordion/Accordion.js
@@ -4,15 +4,25 @@ import { useTranslation } from 'react-i18next';
 const Accordion = ({ title, description, expand, onClick }) => {
   const { t } = useTranslation();
 
+  const isArray = Array.isArray(description);
+
   return (
     <div>
       <dt className={expand ? 'title is-expanded' : 'title'} onClick={onClick}>
         {t(title)}
       </dt>
       <dd className={expand ? 'content is-expanded' : 'content'}>
-        {description.map((elem) => (
-          <p dangerouslySetInnerHTML={{ __html: t(elem) }} />
-        ))}
+        {isArray ? (
+          description.map((elem) => (
+            <p dangerouslySetInnerHTML={{ __html: t(elem) }}></p>
+          ))
+        ) : (
+          <p
+            dangerouslySetInnerHTML={{
+              __html: t(description)
+            }}
+          ></p>
+        )}
       </dd>
     </div>
   );
