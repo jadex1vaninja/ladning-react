afdd638bd10711a25240ef6a26826087d2776b66
jadex1vaninja <ivan.lazarev@jadexconsulting.com>
2021-05-06 10:04:44 +0300

    fix for date

--- src/components/FeaturedNFT/FeaturedNFT.js
+++ src/components/FeaturedNFT/FeaturedNFT.js
@@ -73,12 +73,20 @@ const FeaturedNFT = () => {
     <div className='featured' id='auction'>
       <div className='featured__inner-wrapper'>
         <div className='featured__img-wrapper'>
-        <video controlslist="nodownload" loop muted playsinline="" autoplay="" poster={imgPath + 'Legendary_2_Knockout_Canelo_1_1.png'}>
-          <source src="https://storage.opensea.io/files/9dde6c08550efb852ee28ccded80ad3b.mp4"/>
-          <img
+          <video
+            controlslist='nodownload'
+            loop
+            muted
+            playsinline=''
+            autoplay=''
+            poster={imgPath + 'Legendary_2_Knockout_Canelo_1_1.png'}
+          >
+            <source src='https://storage.opensea.io/files/9dde6c08550efb852ee28ccded80ad3b.mp4' />
+            <img
               src={imgPath + 'Legendary_2_Knockout_Canelo_1_1.png'}
-              alt='card'/>
-        </video>
+              alt='card'
+            />
+          </video>
         </div>
         <div className='featured__content'>
           <h6 className='featured__sub-title'>{t('featured-nft.sub-title')}</h6>
@@ -94,16 +102,14 @@ const FeaturedNFT = () => {
               <h2 className='featured__price-title'>
                 {t('featured-nft.price.title')}
               </h2>
-              <p className='featured__price-value'>
-              {ethPrice.toFixed(3)}
-              </p>
+              <p className='featured__price-value'>{ethPrice.toFixed(3)}</p>
               <p className='featured__price-sign'>
                 {t('featured-nft.price.sign')}
               </p>
               <p>
-              <small class="featured__price-dollar">
-              {`($${usdPrice.toFixed(2)})` }
-              </small>
+                <small class='featured__price-dollar'>
+                  {`($${usdPrice.toFixed(2)})`}
+                </small>
               </p>
             </div>
             <div className='featured__countdown-block'>
@@ -111,16 +117,21 @@ const FeaturedNFT = () => {
                 {t('featured-nft.countdown.title')}
               </h2>
               <Countdown
-                date={highestBid.closing_date}
+                date={createdOrder.closing_date}
                 daysInHours={true}
                 renderer={renderer}
               />
             </div>
           </div>
           <div className='featured__CTA-wrap'>
-            <button onClick={()=>{
-              window.open(LINK_TO_LIVE + FeaturedNFT_ID, "_blank")
-            }} className='featured__CTA'>{t('featured-nft.CTA')}</button>
+            <button
+              onClick={() => {
+                window.open(LINK_TO_LIVE + FeaturedNFT_ID, '_blank');
+              }}
+              className='featured__CTA'
+            >
+              {t('featured-nft.CTA')}
+            </button>
           </div>
         </div>
       </div>
