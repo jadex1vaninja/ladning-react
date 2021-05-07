80a52462a99036c7de5e9bae9d84a1243f86ca89
jadex1vaninja <ivan.lazarev@jadexconsulting.com>
2021-05-07 22:49:50 +0300

    Translations

--- src/pages/PrivacyPolicyPage/PrivacyPage.js
+++ src/pages/PrivacyPolicyPage/PrivacyPage.js
@@ -4,23 +4,24 @@ import Footer from '../../components/Footer';
 import './PrivacyPage.scss';
 import { useHistory } from 'react-router';
 import { PrivacyContent } from './Content';
+import { useTranslation } from 'react-i18next';
 
 const PrivacyPage = () => {
   const history = useHistory();
-
+  const { t } = useTranslation();
   const getBack = () => {
     history.push('/');
   };
   return (
     <>
-      <Header isUsedOnSecondaryPage secondaryTitle='Privacy Policy' />
+      <Header isUsedOnSecondaryPage secondaryTitle={t('terms.CTA-two')} />
       <div className='privacy'>
         <div className='privacy__wrapper'></div>
         <div className='privacy__container'>
-          <p className='privacy__tittle'>Privacy Policy</p>
+          <p className='privacy__tittle'>{t('terms.CTA-two')}</p>
           <div className='splitter'></div>
           <PrivacyContent></PrivacyContent>
-         <div className='privacy__btn-wrapper'>
+          <div className='privacy__btn-wrapper'>
             <button className='privacy__btn' onClick={getBack}>
               GO HOME
             </button>
