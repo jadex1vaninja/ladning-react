49722feb0f5b1f05e5ea10108a1230720ce8c397
jadex1vaninja <ivan.lazarev@jadexconsulting.com>
2021-05-12 18:27:03 +0300

    lang switcher has been hidden

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -49,10 +49,10 @@ const Header = ({
           <h1 className='header__secondary-title'>{secondaryTitle}</h1>
           <ul
             className='header__language-list-secondary'
-            // style={{ visibility: 'hidden' }}
+            style={{ visibility: 'hidden' }}
           >
             <li
-              // style={{ visibility: 'hidden' }}
+              style={{ visibility: 'hidden' }}
               className={`header__button${language === 'en' ? ' active' : ''}`}
               onClick={() => {
                 changeLanguage('en');
@@ -62,7 +62,7 @@ const Header = ({
               En
             </li>
             <li
-              // style={{ visibility: 'hidden' }}
+              style={{ visibility: 'hidden' }}
               className={`header__button${language === 'es' ? ' active' : ''}`}
               onClick={() => {
                 changeLanguage('es');
@@ -113,7 +113,7 @@ const Header = ({
           <div className='header__terms-language'>
             <ul className='header__language-list'>
               <li
-                // style={{ visibility: 'hidden' }}
+                style={{ visibility: 'hidden' }}
                 className={`header__button${
                   language === 'en' ? ' active' : ''
                 }`}
@@ -125,7 +125,7 @@ const Header = ({
                 En
               </li>
               <li
-                // style={{ visibility: 'hidden' }}
+                style={{ visibility: 'hidden' }}
                 className={`header__button${
                   language === 'es' ? ' active' : ''
                 }`}
@@ -147,7 +147,7 @@ const Header = ({
             {t('header.description.text')}
           </p>
           {/* TODO: ADD LINK */}
-          {!showMore  && (
+          {!showMore && (
             <span
               onClick={() => setShowMore(true)}
               className='header__description-link'
@@ -155,7 +155,7 @@ const Header = ({
               {t('header.description.link')}
             </span>
           )}
-          {showMore  && (
+          {showMore && (
             <>
               <br></br>
               <p className='header__description-text'>
